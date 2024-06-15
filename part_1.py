Somehow the normalization did not work when I commited. It is better to say that it worked, but only for the first column. Don't know how that happened. I tried to fix it and it did not work with linalg.norm(), so I've decided to take another approach and try .argmax() normalization method.

part_1.py
@@ -14,8 +14,8 @@ def eigen_decomposition(matrix):
 
     normalized_eigenvectors = np.zeros_like(eigenvectors)
     for i in range(len(eigenvectors)):
-        norm = np.linalg.norm(eigenvectors[:, i])
-        normalized_eigenvectors[:, i] = eigenvectors[:, i] / norm
+        max_value_index = np.argmax(np.abs(eigenvectors[:, i]))
+        normalized_eigenvectors[:, i] = eigenvectors[:, i] / eigenvectors[max_value_index, i]
 
     return eigenvalues, normalized_eigenvectors
 