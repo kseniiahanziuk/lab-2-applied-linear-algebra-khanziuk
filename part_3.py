30175b7c8e31dc3ae10a0609adc96e01c8d908e6
Kseniia Hanziuk <khanziuk@kse.org.ua>
2024-06-16 11:34:34 +0300

    Small typo fixed.

--- part_3.py
+++ part_3.py
@@ -4,8 +4,8 @@ import numpy as np
 def encrypt_message(message, key_matrix):
     message_vector = np.array([ord(char) for char in message])
     eigenvalues, eigenvectors = np.linalg.eig(key_matrix)
-    diaogonalized_key_matrix = np.dot(np.dot(eigenvectors, np.diag(eigenvalues)), np.linalg.inv(eigenvectors))
-    encrypted_vector = np.dot(diaogonalized_key_matrix, message_vector)
+    diagonalized_key_matrix = np.dot(np.dot(eigenvectors, np.diag(eigenvalues)), np.linalg.inv(eigenvectors))
+    encrypted_vector = np.dot(diagonalized_key_matrix, message_vector)
     return encrypted_vector
 
 
